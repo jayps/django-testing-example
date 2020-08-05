9ba4b91d28d462daa610bff33c4597734219feb8
JP Meyer <jp@jpmeyer.dev>
2020-08-05 18:20:27 +0200

    Removed unnecessary code.

--- djangotesting/tests/test_models.py
+++ djangotesting/tests/test_models.py
@@ -4,10 +4,6 @@ from djangotesting.api.models import Person
 
 
 class PersonTest(TestCase):
-    def setUp(self) -> None:
-        # Do some setup stuff here
-        pass
-
     def test_should_return_greeting(self):
         person = Person.objects.create(first_name="John", last_name="Smith")
         greeting = person.get_greeting()
